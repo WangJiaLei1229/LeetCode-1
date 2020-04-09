f06c19b88215210729795f464ad4d28ac29d548e
WangJiaLei <55794401+WangJiaLei1229@users.noreply.github.com>
2020-04-09 17:40:35 +0800

    2020.04.09
    你写的结果不对，你在初始化的时候就已经不对。这是我从LeetCode官网上找到关于并查集的解法，望你修改，谢谢

--- src/leetcode/graph_theory/leet_zh_200/Solution2.java
+++ src/leetcode/graph_theory/leet_zh_200/Solution2.java
@@ -1,88 +1,114 @@
 package leetcode.graph_theory.leet_zh_200;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+package Demo08;
 
-public class Solution2 {
-    /*
-     * 200. 岛屿的个数
-     * 并查集
-     * 执行用时 : 5 ms, 在Number of Islands的Java提交中击败了67.76% 的用户
-     * 内存消耗 : 40.1 MB, 在Number of Islands的Java提交中击败了93.94% 的用户
+public class UnionFind02 {
+	    int count; // # of connected components
+	    int[] parent;
+	    int[] rank;
+	    
+		public  UnionFind02(char[][] grid) { // for problem 200
+	      count = 0;
+	      int m = grid.length;
+	      int n = grid[0].length;
+	      parent = new int[m * n];
+	      rank = new int[m * n];
+	      for (int i = 0; i < m; ++i) {
+	        for (int j = 0; j < n; ++j) {
+	          if (grid[i][j] == '1') {
+	            parent[i * n + j] = i * n + j;
+	            count++;
+	          }
+	          rank[i * n + j] = 0;
+	        }
+	      }
+	    }
+	    //寻找父结点
+	    public int find(int i) { // path compression
+	      if (parent[i] != i) parent[i] = find(parent[i]);
+	      return parent[i];
+	    }
+	    //合并两个集合
+	    public void union(int x, int y) { // union with rank
+	      int rootx = find(x);
+	      int rooty = find(y);
+	      if (rootx != rooty) {
+	        if (rank[rootx] > rank[rooty]) {
+	          parent[rooty] = rootx;
+	        } else if (rank[rootx] < rank[rooty]) {
+	          parent[rootx] = rooty;
+	        } else {
+	          parent[rooty] = rootx; rank[rootx] += 1;
+	        }
+	        count--;
+	      }
+	    }
+	    public int getCount() {
+	      return count;
+	    }
+	  
+	    //判断值的 
+	  public int numIslands(char[][] grid) {
+	    if (grid == null || grid.length == 0) {
+	      return 0;
+	    }
+	    int nr = grid.length;
+	    int nc = grid[0].length;
+	    UnionFind02 uf = new UnionFind02(grid);
+	    for (int r = 0; r < nr; ++r) {
+	      for (int c = 0; c < nc; ++c) {
+	        if (grid[r][c] == '1') {
+	          grid[r][c] = '0';
+	          if (r - 1 >= 0 && grid[r-1][c] == '1') {
+	            uf.union(r * nc + c, (r-1) * nc + c);
+	          }
+	          if (r + 1 < nr && grid[r+1][c] == '1') {
+	            uf.union(r * nc + c, (r+1) * nc + c);
+	          }
+	          if (c - 1 >= 0 && grid[r][c-1] == '1') {
+	            uf.union(r * nc + c, r * nc + c - 1);
+	          }
+	          if (c + 1 < nc && grid[r][c+1] == '1') {
+	            uf.union(r * nc + c, r * nc + c + 1);
+	          }
+	        }
+	      }
+	    }
 
-     * */
+	    return uf.getCount();
+	  }
+	  public static void main(String[] args) {
+		       char[][] grid1 = {
 
-    private class UnionFind {
-        public int[] parent;
+		               {'1', '1', '1', '1', '0'},
 
-        public UnionFind(int n) {
-            parent = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-            }
-        }
+		               {'1', '1', '0', '1', '0'},
 
-        public int find(int a) {
-            if (a < 0 && a >= parent.length) {
-                throw new IllegalArgumentException("a is out of bound");
-            }
+		               {'1', '1', '0', '0', '0'},
 
-            while (a != parent[a]) {
-                a = parent[a];
-            }
-            return a;
-        }
+		              {'0', '0', '0', '0', '0'}};
 
-        public void conn(int a, int b) {
-            int findA = find(a);
-            int findB = find(b);
-            if (findA == findB)
-                return;
-            if (findA < findB)
-                parent[findB] = findA;
-            else
-                parent[findA] = findB;
-        }
-    }
+		       char[][] grid2 = {
 
-    public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
-        int h = grid.length;
-        int w = grid[0].length;
-        UnionFind uf = new UnionFind(h * w);
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (grid[i][j] == '1') {
-                    if (i + 1 < h && grid[i + 1][j] == '1') uf.conn(i * w + j, (i + 1) * w + j);
-                    if (j + 1 < w && grid[i][j + 1] == '1') uf.conn(i * w + j, i * w + j + 1);
-                }
-            }
-        }
-        int res = 0;
-        for (int i = 0; i < uf.parent.length; i++) {
-            if (grid[i / w][i % w] == '1' && i == uf.parent[i])
-                res++;
-        }
-        return res;
-    }
+		               {'1', '1', '0', '0', '0'},
 
-    public static void main(String[] args) {
-        Solution2 ss = new Solution2();
-//        char[][] grid = {
-//                {'1', '1', '1', '1', '0'},
-//                {'1', '1', '0', '1', '0'},
-//                {'1', '1', '0', '0', '0'},
-//                {'0', '0', '0', '0', '0'}};
-//        char[][] grid = {
-//                {'1', '1', '0', '0', '0'},
-//                {'1', '1', '0', '0', '0'},
-//                {'0', '0', '1', '0', '0'},
-//                {'0', '0', '0', '1', '1'}};
-        char[][] grid = {
-                {'1', '0', '1', '1', '1'},
-                {'1', '0', '1', '0', '1'},
-                {'1', '1', '1', '0', '1'}};
-        System.out.println(ss.numIslands(grid));
-    }
+		               {'1', '1', '0', '0', '0'},
+
+		               {'0', '0', '1', '0', '0'},
+		  
+		               {'0', '0', '0', '1', '1'}};
+
+		       char[][] grid3 = {
+
+		               {'1', '0', '1', '1', '1'},
+
+		               {'1', '0', '1', '0', '1'},
+
+		               {'1', '1', '1', '0', '1'}};
+		       UnionFind02 ss = new UnionFind02(grid3);
+		       System.out.println(ss.numIslands(grid3));
+
+		   }
+}
+
 }
